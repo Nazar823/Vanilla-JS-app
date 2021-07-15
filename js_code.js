b3ad615f663f97634e0a27123f7704a41a4a2cdf
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-07-15 15:30:15 +0300

    Закончил To Do

--- js_code.js
+++ js_code.js
@@ -1,68 +1,8 @@
 let items_arr = [];
 let current_color = null;
-let colors = ["red", "blue", "green", "#00d669", "#ffa400", "#530cff"]
-
-function add_item(){
-    console.log("текущий цвет", current_color)
-    let inp_text = document.getElementById("input_text");
-    if (inp_text.value === ""){
-        return alert("Форма пустая!");
-    }
-    let myForm = document.getElementById("myForm");
-    let myTask = new Task();
-    let text = inp_text.value;
-    myTask.id=items_arr.length;
-    myTask.text = text;
-    myTask.checked = false;
-    if (current_color !== null){
-        myTask.color = current_color;
-    } else {
-        myTask.color = colors[Math.floor(Math.random()*6)];
-        current_color = myTask.color;
-    }
-    items_arr.push(myTask);
-    let html_text =  '<div class="item" id="item-'+myTask.id+'"><input type="checkbox" id="check-'+myTask.id+'" class="item_check" onchange="changeCheckBox()"><p class="item_text" id="text-'+myTask.id+'"></p></div>';
-    myForm.insertAdjacentHTML("beforebegin",html_text);
-    document.getElementById("text-"+myTask.id).innerHTML = myTask.text;
-    document.getElementById("item-" + myTask.id).style.backgroundColor = current_color;
-    inp_text.value = "";
-    current_color = null;
-    console.log(items_arr)
-}
-
-function setLavand() {
-    current_color = "#530cff";
-}
-function setGreen_W() {
-    current_color = "#00d669";
-}
-function setOrange() {
-    current_color = "#ffa400";
-}
-function setGreen() {
-    current_color = "green";
-}
-function setBlue() {
-    current_color = "blue";
-}
-function setRed() {
-    current_color = "red";
-}
-
-function changeCheckBox(){
-    for (let i = 0; i < items_arr.length; i++){
-        if (document.getElementById("check-" + i).checked){
-            items_arr[i].checked = true;
-            document.getElementById("item-" + i).className = "item_checked";
-            document.getElementById("item-" + i).style.backgroundColor = "";
-        } else {
-            items_arr[i].checked = false;
-            document.getElementById("item-" + i).className = "item";
-            document.getElementById("item-" + i).style.backgroundColor = items_arr[i].color;
-        }
-    }
-    console.log(items_arr);
-}
+const colors = ["red", "blue", "green", "#00d669", "#ffa400", "#530cff"];
+const colorPicker = document.querySelector('.color_bts_div');
+let selectedColorId = null;
 
 class Task {
     id;
@@ -94,4 +34,102 @@ class Task {
     unCheck(){
         this.checked = false;
     }
+}
+colors.forEach((color, index) => {
+    const colorPickerItem = document.createElement('input');
+    colorPickerItem.id = "picker-"+index;
+    colorPickerItem.type = 'button';
+    colorPickerItem.classList.add('color_bts');
+    colorPickerItem.style.backgroundColor = color;
+    colorPickerItem.addEventListener('click', (e) => {
+        if (selectedColorId !== null){
+            document.querySelector("#" + selectedColorId).style.border = "black solid 0px";
+        }
+        e.target.style.border = "black solid 2px"
+        selectedColorId = e.target.id;
+        current_color = e.target.style.backgroundColor;
+    })
+    colorPicker.append(colorPickerItem);
+})
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random()*colors.length)];
+}
+
+function draw_item(myTask) {
+    const div = document.createElement('div');
+    div.id = 'item-' + myTask.id;
+    div.classList.add('item');
+    div.style.backgroundColor = myTask.color;
+    document.querySelector("#items").append(div);
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.checked = myTask.checked;
+    check.id = "check-" + myTask.id;
+    check.classList.add("item_check");
+    check.onchange = function (){
+        changeCheckBox();
+    }
+    div.append(check);
+    const itm_text = document.createElement('p');
+    itm_text.classList.add("item_text");
+    itm_text.id = "text-" + myTask.id;
+    itm_text.textContent = myTask.text;
+    div.append(itm_text);
+}
+
+function create_item(id_arg, text_arg, check_arg, color_arg){
+    let myTask = new Task();
+    let text = text_arg;
+    myTask.id=id_arg;
+    myTask.text = text;
+    myTask.checked = check_arg;
+    myTask.color = color_arg;
+    items_arr.push(myTask);
+    localStorage.setItem("task_list", JSON.stringify(items_arr));
+    draw_item(myTask);
+}
+
+function add_item(){
+    let inp_text = document.querySelector("#input_text");
+    if (inp_text.value === ""){
+        return alert("Форма пустая!");
+    }
+    if (!current_color){
+        current_color = getRandomColor();
+    }
+    create_item(items_arr.length, inp_text.value, false, current_color);
+    inp_text.value = "";
+    current_color = null;
+    if (selectedColorId){
+        document.querySelector("#"+selectedColorId).style.border = "black solid 0px";
+    }
+    selectedColorId = null;
+    console.log(items_arr)
+}
+function changeCheckBox(){
+    for (let i = 0; i < items_arr.length; i++){
+        console.log("i=", i, "   items_arr.length=", items_arr.length);
+        if (document.querySelector("#check-" + i).checked){
+            items_arr[i].checked = true;
+            document.querySelector("#item-" + i).className = "item_checked";
+            document.querySelector("#item-" + i).style.backgroundColor = "";
+        } else {
+            items_arr[i].checked = false;
+            document.querySelector("#item-" + i).className = "item";
+            document.querySelector("#item-" + i).style.backgroundColor = items_arr[i].color;
+        }
+        localStorage.setItem("task_list", JSON.stringify(items_arr));
+    }
+    console.log(items_arr);
+}
+window.onload = function itemsFromLocalStorage(){
+    console.log("Загрузка страницы, пустой массив => ",items_arr);
+    items_arr = JSON.parse(localStorage.getItem("task_list"));
+    if (items_arr === null){
+        items_arr = [];
+    }
+    console.log("Массив получен => " ,items_arr);
+    items_arr.forEach(item => draw_item(item));
+    changeCheckBox();
 }
